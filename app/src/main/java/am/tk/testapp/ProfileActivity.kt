719d0a05e476d116eb96468e125fe3ae229b5415
tushar <tushardynasty@gmail.com>
2019-05-12 16:40:52 +0530

    Data Storage

--- app/src/main/java/am/tk/testapp/ProfileActivity.kt
+++ app/src/main/java/am/tk/testapp/ProfileActivity.kt
@@ -1,12 +1,68 @@
 package am.tk.testapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.*
 
 class ProfileActivity : AppCompatActivity() {
-
+    private val TAG = "ProfileActivity"
+    private var mDatabaseReference : DatabaseReference?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        val intent = intent
+        var number : String = intent.getStringExtra("mobile")
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference()
+        Log.d(TAG, "Databasse :")
+        mDatabaseReference!!.addChildEventListener(object : ChildEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onChildMoved(p0: DataSnapshot, p1: String?) {
+                val value = p0.getValue(String::class.java)
+                if(value.equals(number)){
+                    mDatabaseReference!!.child(number).child("visit_count").addChildEventListener(object :
+                        ChildEventListener {
+                        override fun onCancelled(p0: DatabaseError) {
+
+                        }
+
+                        override fun onChildMoved(p0: DataSnapshot, p1: String?) {
+
+                        }
+
+                        override fun onChildChanged(p0: DataSnapshot, p1: String?) {
+                            var count_value = p0.getValue(String::class.java) as Int + 1
+                            mDatabaseReference!!.child(number).child("visit_count").setValue(count_value)
+                        }
+
+                        override fun onChildAdded(p0: DataSnapshot, p1: String?) {
+                            //To change body of created functions use File | Settings | File Templates.
+                        }
+
+                        override fun onChildRemoved(p0: DataSnapshot) {
+                            //To change body of created functions use File | Settings | File Templates.
+                        }
+                    })
+
+                }
+
+            }
+
+            override fun onChildChanged(p0: DataSnapshot, p1: String?) {
+
+            }
+
+            override fun onChildAdded(p0: DataSnapshot, p1: String?) {
+
+            }
+
+            override fun onChildRemoved(p0: DataSnapshot) {
+
+            }
+        })
     }
 }
--- app/src/main/java/am/tk/testapp/VerifyActivity.kt
+++ app/src/main/java/am/tk/testapp/VerifyActivity.kt
@@ -7,63 +7,35 @@ import android.os.Bundle
 import android.support.design.widget.CoordinatorLayout
 import android.support.design.widget.Snackbar
 import android.text.Editable
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.TaskExecutors
 import com.google.firebase.FirebaseException
 import com.google.firebase.auth.*
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_verify.*
 import java.util.concurrent.TimeUnit
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DataSnapshot
-
 
 
 class VerifyActivity : AppCompatActivity() {
+    private val TAG = "VerifyActivity"
     private var mVerificationId : String = ""
     private var mAuth : FirebaseAuth ?= null
-    private var mDatabaseReference : DatabaseReference ?= null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_verify)
-        mDatabaseReference = FirebaseDatabase.getInstance().getReference()
+
         mAuth = FirebaseAuth.getInstance()
 
         val intent = intent
         var number : String = intent.getStringExtra("mobile")
-        var numberRef : DatabaseReference = mDatabaseReference!!.child("known_visitors")
-        mDatabaseReference!!.child("known_visitors").addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
 
-                val value = dataSnapshot.getValue(String::class.java)
-                if(value.equals(number)){
-                    mDatabaseReference!!.child("known_visitors").child(number).child("visit_count").addValueEventListener(object : ValueEventListener {
-                        override fun onCancelled(p0: DatabaseError) {
-                            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                        }
-
-                        override fun onDataChange(dataSnapshot: DataSnapshot) {
-                            var count_value = dataSnapshot.getValue(String::class.java) as Int + 1
-                            mDatabaseReference!!.child("known_visitors").child(number).child("visit_count").setValue(count_value)
-                        }
-                    })
-                }
-                else{
-                    sendVerificationCode(number)
-                }
-
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-
-            }
-        })
-
+        Log.d(TAG, "send :")
+        sendVerificationCode(number)
 
 
     }
@@ -119,6 +91,7 @@ class VerifyActivity : AppCompatActivity() {
                     if (task.isSuccessful) {
                         //verification successful we will start the profile activity
                         val intent = Intent(this@VerifyActivity, ProfileActivity::class.java)
+                        intent.putExtra("mobile", credential.provider)
                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
                         startActivity(intent)
 
